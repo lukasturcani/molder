8ce873be1801a94aee17669b1a73d14d4c270d4e
Lukas Turcani <lukasturcani93@gmail.com>
2017-11-27 13:37:37 +0000

    added iodine size

--- js/GLmol.js
+++ js/GLmol.js
@@ -51,7 +51,7 @@ GLmol.prototype.create = function(id, suppressAutoload) {
 // Reference: A. Bondi, J. Phys. Chem., 1964, 68, 441.
    this.vdwRadii = {"H": 1.2, "LI": 1.82, "NA": 2.27, "K": 2.75, "C": 1.7, "N": 1.55, "O": 1.52,
                    "F": 1.47, "P": 1.80, "S": 1.80, "CL": 1.75, "BR": 1.85, "SE": 1.90,
-                   "ZN": 1.39, "CU": 1.4, "NI": 1.63};
+                   "ZN": 1.39, "CU": 1.4, "NI": 1.63, "I": 1.98};
 
    this.id = id;
    this.aaScale = 1; // or 2
